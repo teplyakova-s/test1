953f89a85873cbe89a88e19c6881337187ee28ea
Svetlana.Teplyakova <Svetlana.Teplyakova@jetbrains.com>
2021-07-08 15:57:37 +0300

    Hello1 commit

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello");// write your code here
+        System.out.println("Hello1");// write your code here
     }
 }
+
+
 
